cd8ba80a616a390f58777925031ccf38dcb5b8bd
jbink <jbink17@gmail.com>
2017-05-08 16:23:11 +0900

    configureClient

--- okhttplibrary/src/main/java/jbink/appnapps/okhttplibrary/ApiCall.java
+++ okhttplibrary/src/main/java/jbink/appnapps/okhttplibrary/ApiCall.java
@@ -1,6 +1,15 @@
 package jbink.appnapps.okhttplibrary;
 
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -28,5 +37,45 @@ public class ApiCall {
                 .build();
         Response response = client.newCall(request).execute();
         return response.body().string();
+    }
+
+    public static OkHttpClient.Builder configureClient(final OkHttpClient.Builder builder) {
+        final TrustManager[] certs = new TrustManager[]{new X509TrustManager() {
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            @Override
+            public void checkServerTrusted(final X509Certificate[] chain,
+                                           final String authType) throws CertificateException {
+            }
+
+            @Override
+            public void checkClientTrusted(final X509Certificate[] chain,
+                                           final String authType) throws CertificateException {
+            }
+        }};
+
+        SSLContext ctx = null;
+        try {
+            ctx = SSLContext.getInstance("TLS");
+            ctx.init(null, certs, new SecureRandom());
+        } catch (final java.security.GeneralSecurityException ex) {
+        }
+
+        try {
+            final HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+                @Override
+                public boolean verify(final String hostname, final SSLSession session) {
+                    return true;
+                }
+            };
+            builder.sslSocketFactory(ctx.getSocketFactory()).hostnameVerifier(hostnameVerifier);
+        } catch (final Exception e) {
+        }
+
+        return builder;
     }
 }
